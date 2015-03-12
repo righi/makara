3ceb714890180643eea6af16afe81d5e593b5938
Mike Nelson <mike@mnelson.io>
2015-03-12 14:16:56 -0400

    rollback rack for 1.8.7

--- spec/middleware_spec.rb
+++ spec/middleware_spec.rb
@@ -56,7 +56,7 @@ describe Makara::Middleware do
 
     status, headers, body = middleware.call(env)
 
-    expect(headers['Set-Cookie']).to eq("#{key}=#{Makara::Context.get_current}--200; path=/; max-age=5; HttpOnly")
+    expect(headers['Set-Cookie']).to include("#{key}=#{Makara::Context.get_current}--200; path=/; max-age=5")
   end
 
   it 'should preserve the same context if the previous request was a redirect' do
